7883c9fe8f2ce4e5a0dd4f39979bb74a95aa3b13
sumelinadar <sumelinadar305@gmail.com>
2017-06-27 00:11:33 +0530

    Update CacheDelete.py

--- CacheDelete.py
+++ CacheDelete.py
@@ -9,4 +9,6 @@ class CacheDelete:
         cache = g.GetInformation().getInfo('input/cache_file.tsv')
         [after_cache.append(value) for value in cache if student_id[0] != value[0]]
         wf.WriteFile('input/cache_file.tsv').writeInFile(after_cache, m.Main().headers)
+        if len(cache) == len(after_cache):
+            return m.Main().warning + str(student_id[0]) + " student record does not exist to delete" + m.Main().end
         return m.Main().update + str(student_id[0]) + " student record deleted" + m.Main().end
